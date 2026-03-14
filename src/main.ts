4e278c85f3155847495c8e42c1062f8d395f8a3d
lass04 <yassinelassoued58@gmail.com>
2026-03-14 01:45:37 +0100

    Add authGuard

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // Consider DTO's constraints with ValidationPipe
+  app.useGlobalGuards(new AuthGuard()); // Use an auth guard from ./guards/auth.guard
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
