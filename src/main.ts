c88c4f25572f94faaa45fc652e8680757d1ad901
lass04 <yassinelassoued58@gmail.com>
2026-03-14 21:07:08 +0100

    CORS Configuration

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // Consider DTO's constraints with ValidationPipe
   //app.useGlobalGuards(new AuthGuard()); // Use an auth guard from ./guards/auth.guard
+  app.enableCors({
+    origin : 'http://localhost:5173', // Allowed Requests Origin
+    methods: ['GET','POST','PUT','DELETE'], // HTTP Methods
+    allowHeaders: ['Content-Type','Authorization'], // Allowed request Headers
+    exposedHeaders: ['Content-Range','X-Total-Count'], // Response Headers
+    crendentials:true // Request must contain tokens or headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
