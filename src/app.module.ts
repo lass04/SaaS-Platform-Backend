d96a09501baa75eb4db0efd4d73623e5dc02cb40
lass04 <yassinelassoued58@gmail.com>
2026-03-15 09:31:21 +0100

    User Module creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompaniesModule } from './companies/companies.module';
 import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CompaniesModule, DatabaseModule],
+  imports: [CompaniesModule, DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
