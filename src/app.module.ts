dd589f425e0a1010a030dab4b30b0209ce8c98b0
lass04 <yassinelassoued58@gmail.com>
2026-03-18 00:42:32 +0100

    Rate Limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,43 @@ import { UsersModule } from './users/users.module';
 import { CategoriesModule } from './categories/categories.module';
 import { CustomersModule } from './customers/customers.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerModule,ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [CompaniesModule, DatabaseModule, UsersModule, CategoriesModule, CustomersModule, CustomersModule, AuthModule],
+  imports: [
+    CompaniesModule,
+    DatabaseModule,
+    UsersModule, 
+    CategoriesModule, 
+    CustomersModule, 
+    CustomersModule, 
+    AuthModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 10
+      },
+      {
+        name: 'short',
+        ttl: 100000,
+        limit: 100
+      },
+  ])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
