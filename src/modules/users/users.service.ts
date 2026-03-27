940a638c005b0a220cbaa1d296a5f6998fd6d02a
lass04 <yassinelassoued58@gmail.com>
2026-03-27 19:19:40 +0100

    v2: users module updates

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,5 +1,5 @@
 import { DatabaseService } from '../database/database.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -9,37 +9,47 @@ export class UsersService {
 
   constructor(private readonly db: DatabaseService){}
 
-  async create(user: CreateUserDto) {
-    return await this.db.user.create({ data: user });
-  }
-
-  async findAll() {
-    return await this.db.user.findMany();
-  }
-
-  async findOne(id: string) {
-    return await this.db.user.findUnique({
-      where: { id }
-    });
-  }
-
-   async findByEmail(email: string) {
-    return await this.db.user.findUnique({
-      where: { email }
-    });
-  }
-
-  async update(id: string, updateUserDto: UpdateUserDto) {
-    return await this.db.user.update({
-      where: { id },
-      data: updateUserDto
-    });
-  }
-
-  async remove(id: string) {
-    return await this.db.user.delete({
-      where: { id }
-    });
-  }
+ 
+     async create(dto: CreateUserDto) {
+       const created = await this.db.user.create({ data: dto });
+       if(!created)
+         throw new BadRequestException('Bad request')
+       return created;
+     }
+   
+     async findAll() {
+       const finds = await this.db.user.findMany(); 
+       if(!finds)
+         throw new NotFoundException('Not found');
+       return finds;
+     }
+   
+     async findOne(id: string) {
+       const find = await this.db.user.findUnique({
+         where: { id }
+       });
+       if(!find)
+         throw new NotFoundException('Not found');
+       return find;
+     }
+   
+     async update(id: string, dto: UpdateUserDto) {
+       const updated = await this.db.user.update({
+         where: { id },
+         data: dto
+       });
+       if(!updated)
+         throw new NotFoundException('Not found');
+       return(200);
+     }
+   
+     async remove(id: string) {
+       const removed = await this.db.user.delete({
+         where: { id }
+       });
+       if(!removed)
+         throw new NotFoundException('Not found');
+       return(204);
+     }
 
 }
