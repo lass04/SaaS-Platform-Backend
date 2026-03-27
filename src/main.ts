ac9df66e1ede3d2b99db5d5a67075935ef3bfda5
lass04 <yassinelassoued58@gmail.com>
2026-03-27 19:43:40 +0100

    v2: Cors Cofiguration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
-import { AuthGuard } from './common/guards/auth.guard';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.use(helmet());
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: ['GET','PATCH','POST','DELETE'],
+    allowedHeaders: ['Content-Type','Authorization'],
+    exposedHeaders: ['Content-Range','X-Toatl-Count'],
+    maxAge:600,
+    credentials: true
+  })
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
